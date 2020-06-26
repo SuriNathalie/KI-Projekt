b66ee611cb2632f77e9df66c604e78f12ed8d022
SuriNathalie <65183340+SuriNathalie@users.noreply.github.com>
2020-06-26 19:59:03 +0200

    mit 3D zeichnung

--- V1.py
+++ V1.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jun 22 17:26:36 2020
-
-@author: Suri
-"""
 import numpy as np
 import matplotlib.pyplot as plt
 import warnings
@@ -11,6 +5,8 @@ from collections import Counter
 from matplotlib import style
 import random
 import pandas as pd
+import timeit
+from mpl_toolkits.mplot3d import Axes3D
 
 trainingData={'b':[[1,4],[3,9],[6,0]],'r':[[8,4],[5,7],[1,1]]}
 newData=[4,4]
@@ -29,19 +25,21 @@ def k_nearest_neighbour(data,prediction,k=3):
 
 def openData(name):
         if name.endswith("-2d.train.csv"):
-            dataFrame= pd.read_csv(name, header=-1,index_col=False)
+            dataFrame= pd.read_csv(name,header = -1,index_col=False)
             for i in range(dataFrame.shape[0]):
                 if dataFrame.at[i,0] == 1:
                     plt.scatter(dataFrame.at[i,1],dataFrame.at[i,2],color='b')
                 elif dataFrame.at[i,0] == -1:
                     plt.scatter(dataFrame.at[i,1],dataFrame.at[i,2],color='r')
         elif name.endswith("-3d.train.csv"):
-            dataFrame=pd.read_csv(name,header=-1,index_col=False)
+            dataFrame=pd.read_csv(name, header = -1,index_col=False)
+            fig = plt.figure()
+            ax = fig.add_subplot(111,projection='3d')
             for i in range(dataFrame.shape[0]):
                 if dataFrame.at[i,0] == 1:
-                    plt.scatter(dataFrame.at[i,1],dataFrame.at[i,2],dataFrame.at[i,3],color='b')
+                    ax.scatter(dataFrame.at[i,1],dataFrame.at[i,2],dataFrame.at[i,3],marker='o')
                 elif dataFrame.at[i,0] == -1:
-                    plt.scatter(dataFrame.at[i,1],dataFrame.at[i,2],dataFrame.at[i,3],color='r')
+                    ax.scatter(dataFrame.at[i,1],dataFrame.at[i,2],dataFrame.at[i,3],marker='^')
         elif name.endswith("-4d.train.csv"):
             dataFrame= pd.read_csv(name, header=-1,index_col = False)
         elif name.endswith("-10d.train.csv"):
@@ -49,12 +47,15 @@ def openData(name):
         plt.show()
 
 
-#def kNearestNeighbour(data,point,k):
- #   distances = []
-  #  for row in data:
+def kNearestNeighbour(data,point,k):
+    distances = []
+    for i in range(data.shape[0]):
+        distance = np.linalg.norm(np.array(data)-np.array(point))
+        distances.append(distance,data[0])
+        votes = [i[1] for i in sorted (distances)[:k]]
+        result = Counter(votes).most_common(1)[0][0]
+    return result
+        
+        
             
-
-#[[plt.scatter(ii[1],ii[2],color = i) for ii in dataFrame[i]]for i in dataFrame]
-#plt.scatter(newData[0],newData[1],color= a)
-#plt.show()
-openData('bananas-1-2d.train.csv')
+openData('toy-3d.train.csv')
